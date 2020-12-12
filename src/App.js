fbd543c622249dd1af85dfda461886f00a150bbc
babyazalea <usun12@hotmail.com>
2020-12-12 23:14:12 +0900

    using useState

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Person from "./Person/Person";
 
-class App extends Component {
-  state = {
+const app = (props) => {
+  const [personsState, setPersonsState] = useState({
     persons: [
       { name: "taeyang", age: 30 },
       { name: "eunmin", age: 29 },
       { name: "mandoo", age: 100 },
     ],
-  };
+  });
 
-  switchNameHandler = () => {
+  const [otherState, setOtherState] = useState("some other value");
+
+  const switchNameHandler = () => {
     // console.log("Clicked!");
     // DON'T DO THIS: this.state.persons[0].name = "ty"
-    this.setState({
+    setPersonsState({
       persons: [
         { name: "ty", age: 30 },
         { name: "eunmin", age: 29 },
@@ -23,28 +25,26 @@ class App extends Component {
     });
   };
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, im a react app</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-        />
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        >
-          my hobby is steaming my self
-        </Person>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <h1>Hi, im a react app</h1>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person
+        name={personsState.persons[0].name}
+        age={personsState.persons[0].age}
+      />
+      <Person
+        name={personsState.persons[1].name}
+        age={personsState.persons[1].age}
+      />
+      <Person
+        name={personsState.persons[2].name}
+        age={personsState.persons[2].age}
+      >
+        my hobby is steaming my self
+      </Person>
+    </div>
+  );
+};
 
-export default App;
+export default app;
