1fdd70aa013016ee36b143f4739531c157cc8ba8
babyazalea <usun12@hotmail.com>
2020-12-14 19:52:08 +0900

    two way binding by old-style & component CSS styling

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,15 @@
 import React from "react";
 
+import "./Person.css";
+
 const person = (props) => {
   return (
-    <div>
-      <p>
+    <div className="Person">
+      <p onClick={props.click}>
         hi, i'm {props.name}, my age {props.age}
       </p>
       <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name} />
     </div>
   );
 };
