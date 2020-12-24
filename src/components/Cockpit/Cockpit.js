3a4d1ac8a6a0e383a027be7441632c1ce3f9d8a8
babyazalea <usun12@hotmail.com>
2020-12-24 14:47:51 +0900

    decide when useEffect trigger & cleanup work in useEffect

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -5,7 +5,25 @@ import classes from "./Cockpit.css";
 const cockpit = (props) => {
   useEffect(() => {
     console.log("[Cockpit.js] useEffect");
+    const timer = setTimeout(() => {
+      alert("Saved data to cloud");
+    }, 1000);
+    // below return thing is triggered when component render & mount
+    return () => {
+      clearTimeout(timer);
+      console.log("[Cockpit.js] cleanup work in useEffect");
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log("[Cockpit.js] 2nd useEffect");
+    return () => {
+      console.log("[Cockpit.js] cleanup work 2nd in useEffect");
+    };
   });
+
+  // useEffect hook can use second arg for when excute method in useEffect().
+  // if second arg is empty array, that must be excute first time and just once when app rendering.
 
   const assignedClasses = [];
   let btnClass = "";
--- src/container/App.js
+++ src/container/App.js
@@ -18,6 +18,7 @@ class App extends Component {
     ],
     otherState: "some other value",
     showPersons: false,
+    showCockpit: true,
   };
 
   static getDerivedStateFromProps(props, state) {
@@ -97,12 +98,21 @@ class App extends Component {
 
     return (
       <div className={classes.App}>
-        <Cockpit
-          title={this.props.appTitle}
-          showPersons={this.state.showPersons}
-          persons={this.state.persons}
-          clicked={this.togglePersonsHandler}
-        />
+        <button
+          onClick={() => {
+            this.setState({ showCockpit: false });
+          }}
+        >
+          remove cockpit
+        </button>
+        {this.state.showCockpit ? (
+          <Cockpit
+            title={this.props.appTitle}
+            showPersons={this.state.showPersons}
+            persons={this.state.persons}
+            clicked={this.togglePersonsHandler}
+          />
+        ) : null}
         {persons}
       </div>
     );
